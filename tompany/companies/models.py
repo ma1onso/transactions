89758364a9c560daf0c0d33174bbb8055055b01e
Alonso Enrique <alonso.montenegro@intelligenia.com>
2022-04-08 19:51:35 -0500

    add is_orphan field to Company model

--- tompany/companies/models.py
+++ tompany/companies/models.py
@@ -9,3 +9,6 @@ class Company(TimeStampedModel):
     is_active = models.BooleanField(
         default=True
     )
+    is_orphan = models.BooleanField(
+        default=False, help_text="It's refer to a company without name"
+    )
