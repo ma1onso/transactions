0a7c3ced33162de7e28e735c8e97ae234dc466cd
Alonso Enrique <alonso.montenegro@intelligenia.com>
2022-04-09 07:34:57 -0500

    fix app discoverability

--- tompany/transactions/apps.py
+++ tompany/transactions/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
+from django.utils.translation import gettext_lazy as _
+
 
 class TransactionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transactions'
+    name = 'tompany.transactions'
+    verbose_name = _('Transactions')
